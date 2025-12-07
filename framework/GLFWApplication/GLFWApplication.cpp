48d9151f1c081416641ae508f8689590ac8b3ccd
Dominik Müller <dominimu@stud.ntnu.no>
2025-12-08 00:12:20 +0100

    Updated GLFWApplocation to also handle OpenGL init and have the window properties.

--- framework/GLFWApplication/GLFWApplication.cpp
+++ framework/GLFWApplication/GLFWApplication.cpp
@@ -1,12 +1,53 @@
 #include "GLFWApplication.h"
 
-unsigned GLFWApplication::Init()
+unsigned GLFWApplication::init()
 {
     std::cout << "GLFWApplication::Init() called" << std::endl;
     return 0;
 }
 
-void GLFWApplication::Shutdown()
+void GLFWApplication::shutdown()
 {
     std::cout << "GLFWApplication::Shutdown() called" << std::endl;
+}
+
+unsigned GLFWApplication::run()
+{
+    std::cout << "GLFWApplication::run() called" << std::endl;
+    return 0;
+}
+
+void GLFWApplication::initializeOpenGL()
+{
+    // Initialize GLFW
+    if (!glfwInit())
+    {
+        std::cerr << "Failed to initialize GLFW" << std::endl;
+    }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    m_window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, getName().c_str(), nullptr, nullptr);
+    if (!m_window)
+    {
+        std::cerr << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+    }
+
+    glfwMakeContextCurrent(m_window);
+    glfwSetWindowUserPointer(m_window, this);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cerr << "Failed to initialize GLAD" << std::endl;
+    }
+
+    glViewport(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
+    glEnable(GL_DEPTH_TEST);
+
+    std::cout << "OpenGL init complete" << std::endl;
+
+    m_isOpenGLInitialized = true;
 }
--- framework/GLFWApplication/GLFWApplication.h
+++ framework/GLFWApplication/GLFWApplication.h
@@ -1,22 +1,48 @@
+#ifndef GLFWAPPLICATION_H_
+#define GLFWAPPLICATION_H_
+
 #include <iostream>
 #include <string>
+#include <array>
+#include "glad/glad.h"
+#include "GLFW/glfw3.h"
 
 class GLFWApplication
 {
 public:
-    GLFWApplication(const std::string &name, const std::string &version)
-        : _name(name), _version(version) {}
+    GLFWApplication(const std::string& name, const std::string& version)
+        : m_name(name), m_version(version), WINDOW_HEIGHT(600), WINDOW_WIDTH(800)
+    {
+        initializeOpenGL();
+    }
+    GLFWApplication(const std::string& name, const std::string& version, const int windowHeight, const int windowWidth)
+        : m_name(name), m_version(version), WINDOW_HEIGHT(windowHeight), WINDOW_WIDTH(windowWidth)
+    {
+        initializeOpenGL();
+    }
     GLFWApplication() = default;
     virtual ~GLFWApplication() = default;
 
-    virtual unsigned Init();
-    virtual unsigned Run() const = 0;
-    virtual void Shutdown();
+    const int WINDOW_HEIGHT;
+    const int WINDOW_WIDTH;
 
-    std::string GetName() const { return _name; }
-    std::string GetVersion() const { return _version; }
+    virtual unsigned init();
+    virtual unsigned run();
+    virtual void shutdown();
+
+    bool getIsOpenGLInitialized() const { return m_isOpenGLInitialized; }
+    std::string getName() const { return m_name; }
+    std::string getVersion() const { return m_version; }
+    GLFWwindow* getWindow() const { return m_window; }
+
 
 private:
-    std::string _name;
-    std::string _version;
+    std::string m_name;
+    std::string m_version;
+    GLFWwindow* m_window;
+    bool m_isOpenGLInitialized = false;
+
+    void initializeOpenGL();
 };
+
+#endif // !GLFWAPPLICATION_H_
